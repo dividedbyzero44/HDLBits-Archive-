8ee2877d8321e046fe513f549d84182958c20030
dividedbyzero44 <caohongphuc912@gmail.com>
2026-07-06 17:04:32 +0700

    Add files via upload

--- Problem sets/Verilog Language/Basics/Simple wire/simple_wire_tb.v
+++ Problem sets/Verilog Language/Basics/Simple wire/simple_wire_tb.v
@@ -9,11 +9,25 @@ module simple_wire_tb;
     $dumpvars(0, simple_wire_tb);
    
    //test case 1
-    in = 9'd432;
-    #1;
+    in = 9'd0;
+    #10;
     $display(out);
-    
-    
-    $finish;
+   
+   //test case 2
+    in = 9'd1;
+    #30;
+    $display(out);
+   
+   //test case 3
+    in = 9'd0;
+    #5;
+    $display(out);
+
+   //test case 4
+    in = 9'd1;
+    #5;
+    $display(out);
+
+   $finish;
    end
 endmodule
